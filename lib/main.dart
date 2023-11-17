8239c43063dcae0d7f76ccfcc6424c4d43b01765
saiani1 <saiani@naver.com>
2023-11-17 16:10:56 +0900

    feat: naver map로드, 검색창 구현 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NaverMapSdk.instance.initialize(clientId: 'n9cmbys95e');
   runApp(const MyApp());
 }
 
@@ -9,6 +12,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(children: [
+          NaverMap(
+            options: const NaverMapViewOptions(),
+            onMapReady: (controller) {
+              print("네이버 맵 로딩됨!");
+            },
+          ),
+          Positioned(
+            top: 40,
+            left: 20,
+            right: 20,
+            child: TextField(
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amber.shade400, width: 2),
+                  borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade400, width: 2),
+                  borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                prefixIcon: Icon(Icons.search, color: Colors.grey.shade700),
+                hintText: '검색할 장소를 입력하세요.',
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
